396771f406c6171e66b4881da942f9ee86621d48
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-08-28 16:36:31 -0500

    fixing title margins

--- src/components/addPlantForm/addPlantForm.js
+++ src/components/addPlantForm/addPlantForm.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepButton from '@material-ui/core/StepButton';
+import StepConnector from '@material-ui/core';
 
 import './addPlantForm.css';
 
 class AddPlantForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            plantName: '',
+            botanicalName: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('submit');
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+    }
+
     render() {
         return (
             <div>
-                <p>Hello Add Plant Form</p>
+                <Stepper alternativeLabel="true">
+
+                </Stepper>
+                {/*<form>*/}
+
+                {/*    <div className="addPlantName">*/}
+                {/*        <label htmlFor="plantName">Name</label>*/}
+                {/*        <input type="text" value={ this.state.plantName } onChange={ this.handleChange }/>*/}
+                {/*    </div>*/}
+
+                {/*    <div className="addPlantBotanicalName">*/}
+                {/*        <label htmlFor="botanicalName">Botanical Name</label>*/}
+                {/*        <input type="text" value={ this.state.plantName } onChange={ this.handleChange }/>*/}
+                {/*    </div>*/}
+
+                {/*    <button onClick={ this.handleSubmit }>Add Plant</button>*/}
+
+                {/*</form>*/}
+                
             </div>
         );
     }
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 import './navBar.css';
 
 class NavBar extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="navBar">
-                    <h2 className="navBarTitle">Plant App</h2>
-                    {/* <Link to="/" className="navBarTitle"><h2>Plant App</h2></Link> */}
-                    <Link to="/addPlant" className="navBarItems"><h3>Add Plant</h3></Link>
-                    {/* <a className="navBarItems"><h3>Add Plant</h3></a> */}
-                    {/* <a className="navBarItems"><h3>Add Care</h3></a> */}
-                    <Link to="/addCare" className="navBarItems"><h3>Add Care</h3></Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="navBar">
+          {/* <h2 className="navBarTitle">Plant App</h2> */}
+          <Link to="/" className="navBarTitle">
+            <h2>Plant App</h2>
+          </Link>
+          <Link to="/addPlant" className="navBarItems">
+            <h3>Add Plant</h3>
+          </Link>
+          {/* <a className="navBarItems"><h3>Add Plant</h3></a> */}
+          {/* <a className="navBarItems"><h3>Add Care</h3></a> */}
+          <Link to="/addCare" className="navBarItems">
+            <h3>Add Care</h3>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
