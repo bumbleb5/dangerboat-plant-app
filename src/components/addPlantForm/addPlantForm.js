a9b5ef2ec765fc030106d9087c37fb0693c7f857
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-10-31 19:37:24 -0500

    checking if added an event before submitting

--- src/components/addPlantForm/addPlantForm.js
+++ src/components/addPlantForm/addPlantForm.js
@@ -183,7 +183,17 @@ class AddPlantForm extends React.Component {
         }).catch((error) => {
             console.log(error);
         });
-        PantryService.postEventForPlant(plantId, careEvent);
+        // waterEvent: false,
+        //     waterDate: moment().format(),
+        //     fertilizeEvent: false,
+        //     fertilizeDate: moment().format(),
+        //     treatEvent: false,
+        //     treatDate: moment().format(),
+        //     repotEvent: false,
+        //     repotDate: moment().format(),
+        if (this.state.waterEvent || this.state.fertilizeEvent || this.state.treatEvent || this.state.repotEvent) {
+            PantryService.postEventForPlant(plantId, careEvent);
+        }
     }
 
     handleBack = () => {
